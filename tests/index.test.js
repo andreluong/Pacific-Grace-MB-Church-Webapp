3569cd0c798f2517e3ce5155eb89385f74a1cac3
Andre Luong <andreluong1@gmail.com>
2022-04-07 23:44:20 -0700

    added more tests to index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -19,6 +19,18 @@ describe('homepage', () => {
     })
 })
 
+describe('database', () => {
+    it('should GET database page', (done) => {
+        chai.request(server)
+            .get("/database")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    }) 
+})
+
 describe('login', () => {
     it('should GET login page', (done) => {
         chai.request(server)
@@ -64,6 +76,50 @@ describe('login', () => {
     })
 })
 
+describe('logout', () => {
+    it('should GET logout redirection', (done) => {~
+        chai.request(server)
+            .get("/logout")
+            .redirects(0)
+            .end((err,res) => {
+                res.should.have.status(302);
+                res.should.redirectTo("/");
+                done();
+            }) 
+    })
+})
+
+describe('signup', () => {
+    it('should GET signup page', (done) => {
+        chai.request(server)
+            .get("/signup")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    })
+    it('should reject invalid input', (done) => {
+        chai.request(server)
+            .get("/signup")
+            .send({'email':'notanemail', 'password':'bad'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    })
+    it('should finish signup after input', (done) => {
+        chai.request(server)
+            .post("/signup")
+            .end((err, res) => {
+                res.should.be.string;
+                res.should.have.status(200);
+                done();
+            })
+    })
+})
+
 describe('meeting', () => {
     it('should GET meeting page', (done) => {
         chai.request(server)
@@ -83,7 +139,7 @@ describe('meeting', () => {
                 done();
             })
     })
-    it('should GET a unique meeting room page', (done) => {
+    it('should GET a unique meeting room page and redirect to login', (done) => {
         chai.request(server)
             .get('/meeting/room/:room')
             .end((err,res) => {
@@ -108,9 +164,75 @@ describe('donate', () => {
         chai.request(server)
             .get("/transactions")
             .end((err, res) => {
-                res.should.be.html;
                 res.should.have.status(200);
+                res.should.be.html;
                 done();
             })
+    })
+})
+
+describe('roombooking', () => {
+    it('should GET roombooking page', (done) => {
+        chai.request(server)
+            .get("/roombooking")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    })
+})
+
+describe('contact', () => {
+    it('should GET contact page', (done) => {
+        chai.request(server)
+            .get("/contact")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            })
+    })
+})
+
+describe('account', () => {
+    it('should GET account page', (done) => {
+        chai.request(server)
+            .get("/account")
+            .end((err,res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            }) 
+    })
+    it('should POST account page to redirect to account page', (done) => {
+        chai.request(server)
+        .post("/account")
+        .redirects(0)
+        .end((err,res) => {
+            res.should.have.status(302);
+            res.should.redirectTo("/account");
+            done();
+        }) 
+    })
+    it('should POST account page to redirect to edit page', (done) => {
+        chai.request(server)
+        .post("/account")
+        .send({button: "edit"})
+        .redirects(0)
+        .end((err,res) => {
+            res.should.have.status(302);
+            res.should.redirectTo("/account/edit");
+            done();
+        }) 
+    })
+    it('should GET account edit page', (done) => {
+        chai.request(server)
+            .get("/account/edit")
+            .end((err,res) => {
+                res.should.have.status(200);
+                res.should.be.html;
+                done();
+            }) 
     })
 })
